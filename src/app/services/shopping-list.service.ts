6180a91a494d9b55df4978085abedf022e64442c
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-08-18 23:15:51 -0500

    updated serivice and made api interface

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-// import { ApiResponse } from './../interfaces/api-response';
-// import { Item } from './../interfaces/item';
+import { environment } from '../../environments/environment';
+import { ApiResponse } from '../interfaces/api-response';
+import { Item } from '../models/itemModel';
 
 @Injectable({
   providedIn: 'root',
@@ -12,25 +12,31 @@ export class ShoppingListService {
 
   /**
    *  http://localhost:3000/item/
+   *
+   * TIP: Hold Ctrl then hover variables to see the value
    */
   private apiUrl = environment.apiUrl + '/item/';
 
-  // getItems() {
-  //   return this.http.get<ApiResponse>(this.apiUrl);
-  // }
+  getItems() {
+    return this.http.get<ApiResponse>(this.apiUrl);
+  }
 
-  // createItem(shoppingItem: Item) {
-  //   return this.http.post<ApiResponse>(this.apiUrl, shoppingItem);
-  // }
+  getItem(shoppingItemID: string) {
+    return this.http.get<ApiResponse>(`${this.apiUrl}/${shoppingItemID}`);
+  }
 
-  // updateItem(shoppingItemID: string, shoppingItem: Item) {
-  //   return this.http.patch<ApiResponse>(
-  //     this.apiUrl + shoppingItemID,
-  //     shoppingItem
-  //   );
-  // }
+  createItem(shoppingItem: Item) {
+    return this.http.post<ApiResponse>(this.apiUrl, shoppingItem);
+  }
 
-  // deleteItem(shoppingItemID: string) {
-  //   return this.http.delete<ApiResponse>(this.apiUrl + shoppingItemID);
-  // }
+  updateItem(shoppingItemID: string, shoppingItem: Item) {
+    return this.http.patch<ApiResponse>(
+      `${this.apiUrl}/${shoppingItemID}`,
+      shoppingItem
+    );
+  }
+
+  deleteItem(shoppingItemID: string) {
+    return this.http.delete<ApiResponse>(`${this.apiUrl}/${shoppingItemID}`);
+  }
 }
