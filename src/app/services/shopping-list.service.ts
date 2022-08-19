189b549409d1766f92631540f492d5761ada5f69
tyndalesanjay <60053448+tyndalesanjay@users.noreply.github.com>
2022-08-18 23:43:25 -0500

    Sent src files

---
+++ src/app/services/shopping-list.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+// import { ApiResponse } from './../interfaces/api-response';
+// import { Item } from './../interfaces/item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShoppingListService {
+  constructor(private http: HttpClient) {}
+
+  /**
+   *  http://localhost:3000/item/
+   */
+  private apiUrl = environment.apiUrl + '/item/';
+
+  // getItems() {
+  //   return this.http.get<ApiResponse>(this.apiUrl);
+  // }
+
+  // createItem(shoppingItem: Item) {
+  //   return this.http.post<ApiResponse>(this.apiUrl, shoppingItem);
+  // }
+
+  // updateItem(shoppingItemID: string, shoppingItem: Item) {
+  //   return this.http.patch<ApiResponse>(
+  //     this.apiUrl + shoppingItemID,
+  //     shoppingItem
+  //   );
+  // }
+
+  // deleteItem(shoppingItemID: string) {
+  //   return this.http.delete<ApiResponse>(this.apiUrl + shoppingItemID);
+  // }
+}
