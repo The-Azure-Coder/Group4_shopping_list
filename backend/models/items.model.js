fb011590dac414837e5ba18aca223f2b85f1da4a
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-08-19 09:29:25 -0500

    backend changes

--- backend/models/items.model.js
+++ backend/models/items.model.js
@@ -1,10 +1,18 @@
 const db = require("mongoose");
-const Schema = db.Schema
-let itemsSchema = new db.Schema({
-  categoryID: { type: Schema.Types.ObjectId, required: true, ref: 'category' },
-  item_name: { type: String, required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
-});
+const { Schema } = db;
+
+let itemsSchema = new Schema(
+  {
+    categoryID: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "category",
+    },
+    item_name: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+  },
+  { collection: "items" }
+);
 
 module.exports = db.model("items", itemsSchema);
