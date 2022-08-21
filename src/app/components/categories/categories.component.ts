5b8cfab9e33abc16800d68d4dfd36073f80d840c
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-08-21 16:19:22 -0500

    updated category modal func

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,10 +1,12 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
 import { CategoryService } from 'src/app/service/category.service';
-import { MenuController , ModalController } from '@ionic/angular';
+import { MenuController, ModalController } from '@ionic/angular';
 import { Category } from 'src/app/models/categoryModel';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
-import { FormsModule } from '@angular/forms';
+import { Item } from 'src/app/models/itemModel';
+import { ShoppingListService } from 'src/app/service/shopping-list.service';
+import { ApiResponse } from 'src/app/interfaces/api-response';
 
 @Component({
   selector: 'app-categories',
@@ -12,67 +14,81 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
+  @ViewChildren(IonModal) modals: IonModal[];
 
-  @ViewChild(IonModal) modal: IonModal;
-
-  open(){
-    this.menu.enable(true, "first")
-    this.menu.open("first")  
+  open() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
   }
   categories: Category[] = [];
-  getAllCategories(){
-    this.categoryService.getItems().subscribe(results=>{
-      this.categories = results.data;
-    })
+  items: Item[] = [];
 
+  getAllCategories() {
+    this.categoryService.getItems().subscribe((results) => {
+      this.categories = results.data;
+    });
   }
 
-  constructor(private categoryService: CategoryService,private menu: MenuController, private modalCtrl: ModalController) { }
+  getAllItems() {
+    this.itemService
+      .getItems()
+      .subscribe({ next: (resp: ApiResponse) => (this.items = resp.data) });
+  }
+
+  // filterArrayOfObject;
+
+  constructor(
+    private categoryService: CategoryService,
+    private itemService: ShoppingListService,
+    private menu: MenuController,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {
-    this.getAllCategories()
+    this.getAllCategories();
+    this.getAllItems();
   }
-
 
   //function to delete category
-  deleteCategory(id:any){
-  
-    this.categoryService.deleteItem(id).subscribe(()=>{
-      this.categories = this.categories.filter((category)=>{
-        return category._id !== id
-      })
-    
-    })
+  deleteCategory(id: any) {
+    this.categoryService.deleteItem(id).subscribe(() => {
+      this.categories = this.categories.filter((category) => {
+        return category._id !== id;
+      });
+    });
   }
 
-  
   //modal control code
-  name: string; 
+  name: string;
 
   cancel() {
-    this.modal.dismiss(null, 'cancel');
+    this.modals.forEach((modal) => {
+      modal.dismiss(null, 'cancel');
+    });
   }
 
   confirm() {
-    this.modal.dismiss(this.name, 'update');
+    this.modals.forEach((modal) => {
+      modal.dismiss(this.name, 'update');
+    });
   }
 
-  inputValue:Category;
-  editId: string
+  inputValue: string;
+  editId: string;
 
-  passInfo(id:any, data:any){
+  passInfo(id: any, data: any) {
     this.inputValue = data;
-    this.editId = id
-
+    this.editId = id;
   }
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'update') {
-      this.categoryService.updateItem(this.editId, this.inputValue).subscribe(()=>{
-      })
+      this.categoryService
+        .updateItem(this.editId, { category_name: this.inputValue })
+        .subscribe(() => {
+          this.getAllCategories();
+        });
     }
   }
-
-  
 }
